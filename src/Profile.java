b5119ff4228cb83a0c7669f6dcc3c612354b7375
Bowen Chan <Bowenmchan@gmail.com>
2015-12-01 11:29:21 -0800

    Fixed the boxes for rfiends

--- src/Profile.java
+++ src/Profile.java
@@ -27,9 +27,9 @@ public class Profile extends JPanel {
 	public Profile(Controller cont) {
 		//this.cont = cont;
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 50, 54, 0, 0, 50, 0, 0, 0};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 50, 54, 0, 0, 50, 0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		table_friends = new JTable();
@@ -60,7 +60,6 @@ public class Profile extends JPanel {
         };
 		if(cont.model.getFriends() != null){
 			ArrayList<User> friends = cont.model.getFriends();
-                        friends.add(0, cont.model.getUser());
 			for (User s : friends) {
 	
 				Object[] person = {s.id, s.email, s.fname, s.lname,new Boolean(false) };
@@ -70,11 +69,11 @@ public class Profile extends JPanel {
 		
 		scrollPane2 = new JScrollPane();
 		GridBagConstraints gbc_table_1 = new GridBagConstraints();
-		gbc_table_1.gridwidth = 3;
+		gbc_table_1.gridwidth = 5;
 		gbc_table_1.gridheight = 4;
 		gbc_table_1.insets = new Insets(0, 0, 5, 5);
 		gbc_table_1.fill = GridBagConstraints.BOTH;
-		gbc_table_1.gridx = 5;
+		gbc_table_1.gridx = 2;
 		gbc_table_1.gridy = 1;
 		add(scrollPane2, gbc_table_1);
 		
@@ -84,14 +83,14 @@ public class Profile extends JPanel {
 		JLabel lblEmail = new JLabel("Email:");
 		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
 		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
-		gbc_lblEmail.gridx = 9;
+		gbc_lblEmail.gridx = 8;
 		gbc_lblEmail.gridy = 1;
 		add(lblEmail, gbc_lblEmail);
 		
 		JLabel lblEmailR = new JLabel(cont.model.getUser().email);
 		GridBagConstraints gbc_lblEmailR = new GridBagConstraints();
 		gbc_lblEmailR.insets = new Insets(0, 0, 5, 5);
-		gbc_lblEmailR.gridx = 10;
+		gbc_lblEmailR.gridx = 9;
 		gbc_lblEmailR.gridy = 1;
 		add(lblEmailR, gbc_lblEmailR);
 		
@@ -99,7 +98,7 @@ public class Profile extends JPanel {
 		GridBagConstraints gbc_newEmail = new GridBagConstraints();
 		gbc_newEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_newEmail.fill = GridBagConstraints.HORIZONTAL;
-		gbc_newEmail.gridx = 12;
+		gbc_newEmail.gridx = 11;
 		gbc_newEmail.gridy = 1;
 		add(newEmail, gbc_newEmail);
 		newEmail.setColumns(10);
@@ -107,14 +106,14 @@ public class Profile extends JPanel {
 		JLabel lblFirstName = new JLabel("First Name:");
 		GridBagConstraints gbc_lblFirstName = new GridBagConstraints();
 		gbc_lblFirstName.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFirstName.gridx = 9;
+		gbc_lblFirstName.gridx = 8;
 		gbc_lblFirstName.gridy = 2;
 		add(lblFirstName, gbc_lblFirstName);
 		
 		JLabel lblFName = new JLabel(cont.model.getUser().fname);
 		GridBagConstraints gbc_lblFName = new GridBagConstraints();
 		gbc_lblFName.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFName.gridx = 10;
+		gbc_lblFName.gridx = 9;
 		gbc_lblFName.gridy = 2;
 		add(lblFName, gbc_lblFName);
 		
@@ -122,7 +121,7 @@ public class Profile extends JPanel {
 		GridBagConstraints gbc_newFName = new GridBagConstraints();
 		gbc_newFName.insets = new Insets(0, 0, 5, 5);
 		gbc_newFName.fill = GridBagConstraints.HORIZONTAL;
-		gbc_newFName.gridx = 12;
+		gbc_newFName.gridx = 11;
 		gbc_newFName.gridy = 2;
 		add(newFName, gbc_newFName);
 		newFName.setColumns(10);
@@ -130,14 +129,14 @@ public class Profile extends JPanel {
 		JLabel lblLastName = new JLabel("Last Name:");
 		GridBagConstraints gbc_lblLastName = new GridBagConstraints();
 		gbc_lblLastName.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLastName.gridx = 9;
+		gbc_lblLastName.gridx = 8;
 		gbc_lblLastName.gridy = 3;
 		add(lblLastName, gbc_lblLastName);
 		
 		JLabel lblLName = new JLabel(cont.model.getUser().lname);
 		GridBagConstraints gbc_lblLName = new GridBagConstraints();
 		gbc_lblLName.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLName.gridx = 10;
+		gbc_lblLName.gridx = 9;
 		gbc_lblLName.gridy = 3;
 		add(lblLName, gbc_lblLName);
 		
@@ -145,7 +144,7 @@ public class Profile extends JPanel {
 		GridBagConstraints gbc_newLName = new GridBagConstraints();
 		gbc_newLName.insets = new Insets(0, 0, 5, 5);
 		gbc_newLName.fill = GridBagConstraints.HORIZONTAL;
-		gbc_newLName.gridx = 12;
+		gbc_newLName.gridx = 11;
 		gbc_newLName.gridy = 3;
 		add(newLName, gbc_newLName);
 		newLName.setColumns(10);
@@ -158,14 +157,14 @@ public class Profile extends JPanel {
 		JButton btnDeleteFriend = new JButton("Delete Friend");
 		GridBagConstraints gbc_btnDeleteFriend = new GridBagConstraints();
 		gbc_btnDeleteFriend.insets = new Insets(0, 0, 5, 5);
-		gbc_btnDeleteFriend.gridx = 8;
+		gbc_btnDeleteFriend.gridx = 7;
 		gbc_btnDeleteFriend.gridy = 4;
 		add(btnDeleteFriend, gbc_btnDeleteFriend);
 		
 		JButton btnUpdate = new JButton("Update");
 		GridBagConstraints gbc_btnUpdate = new GridBagConstraints();
 		gbc_btnUpdate.insets = new Insets(0, 0, 5, 5);
-		gbc_btnUpdate.gridx = 12;
+		gbc_btnUpdate.gridx = 11;
 		gbc_btnUpdate.gridy = 4;
 		add(btnUpdate, gbc_btnUpdate);
 		
@@ -173,7 +172,7 @@ public class Profile extends JPanel {
 				GridBagConstraints gbc_table = new GridBagConstraints();
 				gbc_table.fill = GridBagConstraints.BOTH;
 				gbc_table.insets = new Insets(0, 0, 5, 0);
-				gbc_table.gridwidth = 13;
+				gbc_table.gridwidth = 12;
 				gbc_table.gridx = 1;
 				gbc_table.gridy = 5;
 				add(scrollPane, gbc_table);
@@ -183,7 +182,7 @@ public class Profile extends JPanel {
 				JButton btnTableUpdate = new JButton("Table Update");
 				GridBagConstraints gbc_btnTableUpdate = new GridBagConstraints();
 				gbc_btnTableUpdate.insets = new Insets(0, 0, 0, 5);
-				gbc_btnTableUpdate.gridx = 12;
+				gbc_btnTableUpdate.gridx = 11;
 				gbc_btnTableUpdate.gridy = 6;
 				add(btnTableUpdate, gbc_btnTableUpdate);
 
